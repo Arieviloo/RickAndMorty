7fc362aa04f3d05b8f207d5d39dee9fa8cc7418e
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-02 10:50:57 -0300

    init project

---
+++ RickAndMorty/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Jadiê on 02/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
