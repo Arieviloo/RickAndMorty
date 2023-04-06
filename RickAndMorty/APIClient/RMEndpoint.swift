f73946c995d7e0dbf3d69a40c732b82ff07dae88
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-06 12:21:48 -0300

    Added files related API

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,15 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Jadiê on 06/04/23.
+//
+
+import Foundation
+
+@frozen enum RMEndpoint: String {
+    case character
+    case location
+    case episode
+    
+}
---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,12 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Jadiê on 06/04/23.
+//
+
+import Foundation
+
+final class RMRequest {
+    
+}
